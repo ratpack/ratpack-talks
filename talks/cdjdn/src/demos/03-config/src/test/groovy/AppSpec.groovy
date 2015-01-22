07deb20d4b3ebff93aa4fda48ecad58dc4cc8b08
David M. Carr <david@carrclan.us>
2015-01-21 22:17:09 -0500

    Test in config demo

--- talks/cdjdn/src/demos/03-config/src/test/groovy/AppSpec.groovy
+++ talks/cdjdn/src/demos/03-config/src/test/groovy/AppSpec.groovy
@@ -1,20 +1,34 @@
+import ratpack.http.internal.HttpHeaderConstants
 import ratpack.test.ApplicationUnderTest
 import ratpack.test.http.TestHttpClients
 import spock.lang.AutoCleanup
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class AppSpec extends Specification {
     @AutoCleanup
     def aut = ApplicationUnderTest.of(App)
-    def client = TestHttpClients.testHttpClient(aut, { rs -> rs.redirects(10) } )
+    def client = TestHttpClients.testHttpClient(aut)
 
-//    void "different greetings by name and content type"() {
-//        when:
-//        def response = client.requestSpec({ rs -> rs.headers( { headers -> hreadesr.} ) }) .get("greet/${name}")
-//
-//        then:
-//
-//        where:
-//        name | contentType |
-//    }
+    @Unroll
+    void "different greetings by name and content type: #name; #contentType"() {
+        when:
+        def response = client.requestSpec({ rs ->
+            rs.headers({ headers ->
+                headers.set(HttpHeaderConstants.ACCEPT, contentType)
+            })
+        }).get("greet/${name}")
+
+        then:
+        response.body.contentType.type == contentType
+        response.body.text == expectedText
+
+        where:
+        name | contentType | expectedText
+        "Leonard" | "text/plain" | "Greetings, Leonard"
+        "Sheldon" | "text/plain" | "Greetings, Sheldon"
+        "Howard" | "text/html" | "Greetings, <b>Howard</b>"
+        "Rajesh" | "text/html" | "Greetings, <b>Rajesh</b>"
+        "Penny" | "application/json" | "{\n  \"greeting\" : \"Greetings,\",\n  \"name\" : \"Penny\"\n}"
+    }
 }
